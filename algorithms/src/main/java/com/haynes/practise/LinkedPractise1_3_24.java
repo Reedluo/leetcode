997e5b8ddea4c12148c64436df40031a315daafc
yangheng <15885314768@163.com>
2020-08-23 22:29:11 +0800

    练习题

---
+++ algorithms/src/main/java/com/haynes/practise/LinkedPractise1_3_24.java
@@ -0,0 +1,28 @@
+package com.haynes.practise;
+
+import edu.princeton.cs.algs4.LinkedStack;
+
+/**
+ * @Description: 1.3.24 編寫一個 removeAfter(),
+ * 接受一个链表节点作为参数并删除该节点的后续节点（如果参数节点或参数节点的后续节点为空则什么也不做)。
+ * @Param:
+ * @return:
+ * @Author: yangheng
+ * @Date: 2020/8/23
+ **/
+public class LinkedPractise1_3_24 {
+    public static void main(String[] args) {
+        LinkedStack<Integer> linkedStack = new LinkedStack<>();
+        for (int i = 0; i < 10; i++) {
+            linkedStack.push(i);
+        }
+        removeAfter(linkedStack);
+        for (Integer val : linkedStack) {
+            System.out.print(val);
+        }
+    }
+
+    private static void removeAfter(LinkedStack<Integer> linkedStack) {
+
+    }
+}
